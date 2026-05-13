fe0c9fffddcb49096565e5e52915fb93f872eb64
Angelooo29 <reymarcmacalibo@gmail.com>
2026-05-14 01:14:57 +0800

    For lab exercise 5

--- lab5/Form1.Designer.cs
+++ lab5/Form1.Designer.cs
@@ -20,20 +20,164 @@
 			base.Dispose(disposing);
 		}
 
-		#region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-		/// <summary>
-		///  Required method for Designer support - do not modify
-		///  the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
-		}
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtAuthorName = new TextBox();
+            txtBookTitle = new TextBox();
+            txtAddBooks = new Button();
+            listBoxBooks = new ListBox();
+            label1 = new Label();
+            label2 = new Label();
+            btnUpdateBook = new Button();
+            txtBookID = new TextBox();
+            label3 = new Label();
+            btnShowBooks = new Button();
+            btnDeleteBooks = new Button();
+            SuspendLayout();
+            // 
+            // txtAuthorName
+            // 
+            txtAuthorName.Location = new Point(191, 147);
+            txtAuthorName.Name = "txtAuthorName";
+            txtAuthorName.Size = new Size(208, 27);
+            txtAuthorName.TabIndex = 2;
+            // 
+            // txtBookTitle
+            // 
+            txtBookTitle.Location = new Point(191, 92);
+            txtBookTitle.Name = "txtBookTitle";
+            txtBookTitle.Size = new Size(208, 27);
+            txtBookTitle.TabIndex = 1;
+            // 
+            // txtAddBooks
+            // 
+            txtAddBooks.BackColor = Color.Cyan;
+            txtAddBooks.Location = new Point(115, 204);
+            txtAddBooks.Name = "txtAddBooks";
+            txtAddBooks.Size = new Size(94, 29);
+            txtAddBooks.TabIndex = 3;
+            txtAddBooks.Text = "Add";
+            txtAddBooks.UseVisualStyleBackColor = false;
+            txtAddBooks.Click += btnAddBooks_Click;
+            // 
+            // listBoxBooks
+            // 
+            listBoxBooks.FormattingEnabled = true;
+            listBoxBooks.Location = new Point(426, 86);
+            listBoxBooks.Name = "listBoxBooks";
+            listBoxBooks.Size = new Size(333, 264);
+            listBoxBooks.TabIndex = 6;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(88, 95);
+            label1.Name = "label1";
+            label1.Size = new Size(76, 20);
+            label1.TabIndex = 7;
+            label1.Text = "Book Title";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(66, 147);
+            label2.Name = "label2";
+            label2.Size = new Size(98, 20);
+            label2.TabIndex = 8;
+            label2.Text = "Author Name";
+            // 
+            // btnUpdateBook
+            // 
+            btnUpdateBook.BackColor = Color.GreenYellow;
+            btnUpdateBook.Location = new Point(115, 321);
+            btnUpdateBook.Name = "btnUpdateBook";
+            btnUpdateBook.Size = new Size(94, 29);
+            btnUpdateBook.TabIndex = 6;
+            btnUpdateBook.Text = "Update";
+            btnUpdateBook.UseVisualStyleBackColor = false;
+            btnUpdateBook.Click += btnUpdateBooks_Click;
+            // 
+            // txtBookID
+            // 
+            txtBookID.Location = new Point(191, 260);
+            txtBookID.Name = "txtBookID";
+            txtBookID.Size = new Size(125, 27);
+            txtBookID.TabIndex = 5;
+            txtBookID.TabStop = false;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(102, 267);
+            label3.Name = "label3";
+            label3.Size = new Size(62, 20);
+            label3.TabIndex = 11;
+            label3.Text = "Book ID";
+            // 
+            // btnShowBooks
+            // 
+            btnShowBooks.BackColor = Color.Aquamarine;
+            btnShowBooks.Location = new Point(250, 204);
+            btnShowBooks.Name = "btnShowBooks";
+            btnShowBooks.Size = new Size(94, 29);
+            btnShowBooks.TabIndex = 4;
+            btnShowBooks.Text = "Show";
+            btnShowBooks.UseVisualStyleBackColor = false;
+            btnShowBooks.Click += btnShowBooks_Click;
+            // 
+            // btnDeleteBooks
+            // 
+            btnDeleteBooks.BackColor = Color.Tomato;
+            btnDeleteBooks.Location = new Point(250, 321);
+            btnDeleteBooks.Name = "btnDeleteBooks";
+            btnDeleteBooks.Size = new Size(94, 29);
+            btnDeleteBooks.TabIndex = 7;
+            btnDeleteBooks.Text = "Delete";
+            btnDeleteBooks.UseVisualStyleBackColor = false;
+            btnDeleteBooks.Click += btnDeleteBooks_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.PeachPuff;
+            ClientSize = new Size(782, 453);
+            Controls.Add(btnDeleteBooks);
+            Controls.Add(btnShowBooks);
+            Controls.Add(label3);
+            Controls.Add(txtBookID);
+            Controls.Add(btnUpdateBook);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(listBoxBooks);
+            Controls.Add(txtAddBooks);
+            Controls.Add(txtBookTitle);
+            Controls.Add(txtAuthorName);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
 
-		#endregion
-	}
+        #endregion
+
+        private TextBox txtAuthorName;
+        private TextBox txtBookTitle;
+        private Button txtAddBooks;
+        private ListBox listBoxBooks;
+        private Label label1;
+        private Label label2;
+        private Button btnUpdateBook;
+        private TextBox txtBookID;
+        private Label label3;
+        private Button btnShowBooks;
+        private Button btnDeleteBooks;
+    }
 }
--- lab5/Form1.cs
+++ lab5/Form1.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace lab5
 {
 	public partial class Form1 : Form
@@ -6,5 +8,127 @@ namespace lab5
 		{
 			InitializeComponent();
 		}
-	}
+
+		public class Author
+		{
+			public int AuthorID { get; set; }
+			public string Name { get; set; }
+			public virtual ICollection<Book> Books { get; set; }
+
+        }
+
+		public class Book
+		{
+			public int BookID { get; set; }
+			public string Title { get; set; }
+			public int AuthorID { get; set; }
+			public virtual Author Author { get; set; }
+        }
+
+		public void AddAuthorWithBook(string authorName, string bookTitle)
+		{
+			using (var context = new BookstoreContext())
+			{
+				var author = new Author { Name = authorName };
+				var book = new Book { Title = bookTitle, Author = author };
+
+				context.Authors.Add(author);
+				context.Books.Add(book);
+				context.SaveChanges();
+            }
+		}
+
+		public List<string> GetBooksWithAuthors()
+		{
+			using (var context = new BookstoreContext())
+			{
+				var booksWithAuthors = context.Books
+					.Include(b => b.Author)
+					.Select(b => $"{b.Title} by {b.Author.Name}")
+					.ToList();
+
+				return booksWithAuthors;
+			}
+		}
+
+		private void btnShowBooks_Click(object sender, EventArgs e)
+		{
+			var books = GetBooksWithAuthors();
+			listBoxBooks.DataSource = books;
+        }
+
+        private void btnAddBooks_Click(object sender, EventArgs e)
+        {
+            string authorName = txtAuthorName.Text;
+            string bookTitle = txtBookTitle.Text;
+
+            if (!string.IsNullOrWhiteSpace(authorName) && !string.IsNullOrWhiteSpace(bookTitle))
+            {
+                AddAuthorWithBook(authorName, bookTitle);
+
+                MessageBox.Show("Book and Author(s) Saved.");
+
+                txtAuthorName.Clear();
+                txtBookTitle.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Please enter the Book title and the Author(s).");
+            }
+        }
+
+        public void UpdateBookAndAuthor(int bookId, string newTitle, string newAuthorName)
+		{
+			using (var context = new BookstoreContext())
+			{
+				var book = context.Books.Include(b => b.Author).FirstOrDefault(b => b.BookID == bookId);
+
+				if (book != null)
+				{
+					book.Title = newTitle;
+					book.Author.Name = newAuthorName;
+					context.SaveChanges();
+                }
+            }
+		}
+
+        private void btnUpdateBooks_Click(object sender, EventArgs e)
+		{
+			int bookId = int.Parse(txtBookID.Text);
+			UpdateBookAndAuthor(bookId, txtBookTitle.Text, txtAuthorName.Text);
+			MessageBox.Show("Book and Author(s) updated successfully.");
+		}
+
+        public void DeleteBook(int bookId)
+        {
+            using (var context = new BookstoreContext())
+            {
+                var bookToDelete = context.Books.FirstOrDefault(b => b.BookID == bookId);
+
+                if (bookToDelete != null)
+                {
+                    context.Books.Remove(bookToDelete);
+                    context.SaveChanges();
+                }
+				
+            }
+        }
+
+        private void btnDeleteBooks_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(txtBookID.Text, out int bookId))
+            {
+                DeleteBook(bookId);
+                MessageBox.Show("Book deleted successfully!");
+
+                btnShowBooks_Click(sender, e);
+
+                txtBookID.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid Book ID to delete.");
+            }
+        }
+    }
 }
